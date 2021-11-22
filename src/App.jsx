79ff9daac42a6407e5dc9243dd5ce7d80a493e99
Thales Nunes <thales.o.nunes@gmail.com>
2021-11-22 17:14:36 -0300

    Inserindo Tasks

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import {v4 as uuidv4} from 'uuid';
+
+import './App.css';
+import { useState } from "react";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+
+const App = () => {
+    const [tasks, setTasks] = useState([
+        {
+            id: '1',
+            title: 'Estudar programação',
+            completed: false
+        },
+        {
+            id: '2',
+            title: 'Ler livros',
+            completed: true
+        }
+
+
+    ]);
+
+    const handleTaskAddition = (taskTitle) => {
+        const newTasks = [... tasks, {
+            title: taskTitle,
+            id: uuidv4(),
+            completed: false
+        }]
+
+        setTasks(newTasks);
+    }
+
+    return <div className="container" >
+        <AddTask handleTaskAddition={handleTaskAddition} />
+        <Tasks tasks={tasks} />
+    </div>
+    
+}
+
+export default App;
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,13 @@
+import react from "react";
+import Task from "./Task";
+
+const Tasks = ({tasks}) => {
+
+    return (
+        <div>
+            {tasks.map(task => <Task task={task}/>)}
+        </div>
+    );
+};
+
+export default Tasks;
